23135060c9d9059fae7bd0deae7b4f53d35e400c
senator423 <oumasenator@gmail.com>
2025-05-10 13:54:37 +0300

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.werescue"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.werescue"
@@ -44,23 +44,29 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.storage)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
-    implementation("com.google.firebase:firebase-auth")
+    implementation(libs.google.firebase.auth)
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     // Add this line to include Firebase Database
-    implementation("com.google.firebase:firebase-database")
+    implementation(libs.google.firebase.database)
 
-    implementation ("com.google.firebase:firebase-database:19.7.0")
+    implementation (libs.firebase.database.v1970)
 
     // Add these lines to include Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
     
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation (libs.com.github.bumptech.glide.glide)
+    annotationProcessor (libs.compiler)
 }
+
+
